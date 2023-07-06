71b18fa21edb21a8f62775f41a67bcc571628a8b
minhasjatinder <91667539+minhasjatinder@users.noreply.github.com>
2023-07-06 22:07:29 +0530

    Update App.py

--- InNews-master/InNews-master/App.py
+++ InNews-master/InNews-master/App.py
@@ -52,7 +52,7 @@ def fetch_news_poster(poster_link):
         image = Image.open(io.BytesIO(raw_data))
         st.image(image, use_column_width=True)
     except:
-        image = Image.open('./Meta/no_image.jpg')
+        image = Image.open('/app/summarized-news-app/InNews-master/InNews-master/no_image.jpg')
         st.image(image, use_column_width=True)
 
 
